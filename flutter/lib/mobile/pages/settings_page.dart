bce328df658953d3c5010ca572a31d824eb5f3d4
XavierAlpha <52537840+XavierAlpha@users.noreply.github.com>
2025-09-09 23:37:14 +0800

    update workflows; change default config; some changes;

--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -106,7 +106,8 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
         bind.mainGetOptionSync(key: kOptionEnableLanDiscovery));
     _onlyWhiteList = whitelistNotEmpty();
     _enableDirectIPAccess = option2bool(
-        kOptionDirectServer, bind.mainGetOptionSync(key: kOptionDirectServer));
+        kOptionEnableDirectServer,
+        bind.mainGetOptionSync(key: kOptionEnableDirectServer));
     _enableRecordSession = option2bool(kOptionEnableRecordSession,
         bind.mainGetOptionSync(key: kOptionEnableRecordSession));
     _enableHardwareCodec = option2bool(kOptionEnableHwcodec,
@@ -172,7 +173,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
       }
 
       var checkUpdateOnStartup =
-          mainGetLocalBoolOptionSync(kOptionEnableCheckUpdate);
+          mainGetLocalBoolOptionSync(kOptionAllowCheckUpdate);
       if (checkUpdateOnStartup != _checkUpdateOnStartup) {
         update = true;
         _checkUpdateOnStartup = checkUpdateOnStartup;
@@ -453,14 +454,15 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
                             }))
             ]),
         initialValue: _enableDirectIPAccess,
-        onToggle: isOptionFixed(kOptionDirectServer)
+        onToggle: isOptionFixed(kOptionEnableDirectServer)
             ? null
             : (_) async {
                 _enableDirectIPAccess = !_enableDirectIPAccess;
                 String value =
-                    bool2option(kOptionDirectServer, _enableDirectIPAccess);
+                    bool2option(
+                    kOptionEnableDirectServer, _enableDirectIPAccess);
                 await bind.mainSetOption(
-                    key: kOptionDirectServer, value: value);
+                    key: kOptionEnableDirectServer, value: value);
                 setState(() {});
               },
       ),
@@ -592,7 +594,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
             Text(translate('Check for software update on startup')),
           ]),
           onToggle: (bool toValue) async {
-            await mainSetLocalBoolOption(kOptionEnableCheckUpdate, toValue);
+            await mainSetLocalBoolOption(kOptionAllowCheckUpdate, toValue);
             setState(() => _checkUpdateOnStartup = toValue);
           },
         ),
