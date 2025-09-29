8e10a1bf5f458ee2e90f364f89b9b3f347f8d817
XavierAlpha <52537840+XavierAlpha@users.noreply.github.com>
2025-09-30 03:32:05 +0800

    change default settings

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -1562,7 +1562,7 @@ bool option2bool(String option, String value) {
     res = value != "N";
   } else if (option.startsWith("allow-") ||
       option == kOptionStopService ||
-      option == kOptionDirectServer ||
+      option == kOptionEnableDirectServer ||
       option == kOptionForceAlwaysRelay) {
     res = value == "Y";
   } else {
@@ -1580,7 +1580,7 @@ String bool2option(String option, bool b) {
     res = b ? defaultOptionYes : 'N';
   } else if (option.startsWith('allow-') ||
       option == kOptionStopService ||
-      option == kOptionDirectServer ||
+      option == kOptionEnableDirectServer ||
       option == kOptionForceAlwaysRelay) {
     res = b ? 'Y' : defaultOptionNo;
   } else {
